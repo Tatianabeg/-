40cac312403b28aab9a172d3dbc0d5bff7d74da9
Tatiana <tatiana.beglarova@gmail.com>
2022-08-26 18:52:13 +0300

    inition file

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,26 +1,36 @@
-﻿Task34.
+﻿Task38.
 Console.WriteLine("size array");
 int size = Convert.ToInt32(Console.ReadLine());
-int[] numbers = new int[size];
+double[] numbers = new double[size];
 FillArrayRandomNumbers(numbers);
 Console.WriteLine("array");
 PrintArray(numbers);
-int count = 0;
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
 
-for (int z = 0; z < numbers.Length; z++);
-if (numbers[z] % 2 == 0 )
-count++;
+for (int z = 0; z < numbers.Length; z++)
+{
+    if (numbers[z]  > max)
+      {
+        max = numbers[z];
+      }
+    if (numbers[z] < min)
+      {
+        min = numbers[z];
+      }
+}
 
-Console.WriteLine($"{numbers.Length},{count}");
+    Console.WriteLine($"{numbers.Length}");
+    Console.WriteLine(${max - min});
 
-void FillArrayRandomNumbers(int[] numbers)
+void FillArrayRandomNumbers(double[] numbers)
 {
     for (int i = 0; i < numbers.Length; i++)
     {
-        numbers[i] = new Random().Next(100, 1000);
+        numbers[i] = Convert.ToDouble(new Random().Next(100, 1000)) / 100;
     }
 }
-void printArray(int[] numbers)
+void printArray(double[] numbers)
 {
     Console.Write("[");
     for(int i = 0; i < numbers.Length; i++)
